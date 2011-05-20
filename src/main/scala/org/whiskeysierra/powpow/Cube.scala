26862542078482efd00f7b7395eb2dec044954a1
Willi Schönborn <schoenborn@cosmocode.de>
2011-05-20 22:12:34 +0200

    changed stuff

--- src/main/scala/org/whiskeysierra/powpow/Cube.scala
+++ src/main/scala/org/whiskeysierra/powpow/Cube.scala
@@ -7,8 +7,10 @@ import scala.actors.Actor
 
 class Cube(private val node:SceneNode) extends Actor {
     
-    var x:Float = 0
-    var y:Float = 0
+    var x:Float = 1
+    var y:Float = 1
+    
+    private var last:Vector2f = new Vector2f
     
     private val speed = 0.01f
     
@@ -25,21 +27,31 @@ class Cube(private val node:SceneNode) extends Actor {
     
     def update():Unit = {
         val current = new Vector2f(x, y)
-        var angle = 0f
         
-        if (current.length > 0) {
-            current.normalize
-            angle = Math.acos(current.dot(new Vector2f(1, 0))).toFloat
-            
-            if (y < 0) {
-                angle = Math.Pi.toFloat * 2f - angle
-            }
+        if (last.epsilonEquals(current, 0.01f)) {
+            return
         }
         
-        node.setTransform(
-            Transform.translate(x, y, 0).mul(
-            Transform.rotateZ(angle)
-        ))
+        val direction = new Vector2f(x, y)
+        direction.sub(last)
+        
+        var angle = 0f
+        
+        if (direction.length > 0) {
+            last = new Vector2f(x, y)
+            direction.normalize
+            angle = Math.acos(direction.dot(new Vector2f(1, 0))).toFloat
+            
+//            if (y < 0) {
+//                angle = Math.Pi.toFloat * 2f - angle
+//            }
+            node.setTransform(
+                    Transform.scale(1.0f).mul(
+                            Transform.translate(x, y, 0).mul(
+                                    Transform.rotateZ(angle)
+                            )))
+        }
+        
     }
     
 }
