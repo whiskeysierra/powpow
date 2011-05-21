778b12bc6de359af30faeeb0db5a989911a789de
Willi Schönborn <willi.schoenborn@gmx.net>
2011-05-21 13:22:25 +0200

    added sealed abstract class

---
+++ src/main/scala/org/whiskeysierra/powpow/Messages.scala
@@ -0,0 +1,16 @@
+package org.whiskeysierra.powpow
+
+sealed abstract class Message
+
+case object Update extends Message
+
+case class MoveX(val delta:Float) extends Message
+case class MoveY(val delta:Float) extends Message
+
+case class Position(val x:Float, val y:Float) extends Message
+
+case class ShootX(val value:Float) extends Message
+case class ShootY(val value:Float) extends Message
+
+case object PoisonPill extends Message
+case object Quit extends Message
