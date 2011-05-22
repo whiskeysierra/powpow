7f6415cdd4cec6d7611fde1203cca17b70b68727
Willi Schönborn <willi.schoenborn@gmx.net>
2011-05-22 22:20:57 +0200

    added Physical trait

--- src/main/scala/org/whiskeysierra/powpow/Bullet.scala
+++ src/main/scala/org/whiskeysierra/powpow/Bullet.scala
@@ -1,55 +1,18 @@
 package org.whiskeysierra.powpow
 
-import com.bulletphysics.collision.dispatch.CollisionObject
-import com.bulletphysics.collision.shapes.SphereShape
-import com.bulletphysics.dynamics.{RigidBodyConstructionInfo, RigidBody}
-import com.bulletphysics.linearmath.{Transform, MotionState}
-import javax.vecmath.{Vector3f, Matrix4f}
+import com.bulletphysics.collision.shapes.{CollisionShape, SphereShape}
+import com.bulletphysics.linearmath.MotionState
 
 object Bullet {
     
     def apply(position:Vector, direction:Vector):Bullet = {
         val radius = .5f
         val shape = new SphereShape(radius)
-        val mass = 1
-        val inertia = new Vector3f
-        shape.calculateLocalInertia(mass, inertia)
-        val bullet = new Bullet(position, direction)
-        val info = new RigidBodyConstructionInfo(mass, bullet, shape, inertia)
-        info.restitution = 0
-        info.linearDamping = 0
-        info.angularDamping = 0
-        val body = new RigidBody(info)
-        val matrix = new Matrix4f
-        matrix.set(position toVector3f)
-        body.proceedToTransform(new Transform(matrix))
-        body.setLinearVelocity(direction * 25 toVector3f);
-        body.setActivationState(CollisionObject.DISABLE_DEACTIVATION)
-        
-        bullet.body = body
-        
-        return bullet
+        val bullet = new Bullet(position, direction, shape)
+        bullet.body.setLinearVelocity(direction * 25 toVector3f)
+        bullet
     }
     
 }
 
-class Bullet(var position:Vector, val direction:Vector) extends MotionState {
-
-    var body:RigidBody = null
-    
-    override def getWorldTransform(transform:Transform) = {
-        val matrix = new Matrix4f
-        matrix.set(position toVector3f)
-        transform.set(matrix)
-        transform
-    }
-
-    override def setWorldTransform(transform:Transform) = {
-        val matrix = new Matrix4f
-        transform.getMatrix(matrix)
-        val translation = new Vector3f
-        matrix.get(translation)
-        position = Vector(translation)
-    }
-    
-}
+class Bullet(var position:Vector, val direction:Vector, val shape:CollisionShape) extends Physical
---
+++ src/main/scala/org/whiskeysierra/powpow/Physical.scala
@@ -0,0 +1,47 @@
+package org.whiskeysierra.powpow
+
+import com.bulletphysics.collision.dispatch.CollisionObject
+import com.bulletphysics.collision.shapes.CollisionShape
+import com.bulletphysics.dynamics.{RigidBodyConstructionInfo, RigidBody}
+import com.bulletphysics.linearmath.{Transform, MotionState}
+import javax.vecmath.{Vector3f, Matrix4f}
+
+trait Physical extends MotionState {
+
+    var position:Vector
+    val body = createBody
+    
+    def shape:CollisionShape
+    
+    private def createBody:RigidBody = {
+        val mass = 1
+        val inertia = new Vector3f
+        shape.calculateLocalInertia(mass, inertia)
+        val info = new RigidBodyConstructionInfo(mass, this, shape, inertia)
+        info.restitution = 0
+        info.linearDamping = 0
+        info.angularDamping = 0
+        val body = new RigidBody(info)
+        val matrix = new Matrix4f
+        matrix.set(position toVector3f)
+        body.proceedToTransform(new Transform(matrix))
+        body.setActivationState(CollisionObject.DISABLE_DEACTIVATION)
+        body
+    }
+    
+    override def getWorldTransform(transform:Transform) = {
+        val matrix = new Matrix4f
+        matrix.set(position toVector3f)
+        transform.set(matrix)
+        transform
+    }
+
+    override def setWorldTransform(transform:Transform) = {
+        val matrix = new Matrix4f
+        transform.getMatrix(matrix)
+        val translation = new Vector3f
+        matrix.get(translation)
+        position = Vector(translation)
+    }
+    
+}
