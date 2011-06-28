ddc52ddb8cc47d4146113222edd062c9af8e2ea9
Willi Schönborn <willi.schoenborn@gmx.net>
2011-06-29 00:34:31 +0200

    modified shaders

---
+++ src/main/scala/org/whiskeysierra/powpow/bloom.fs
@@ -0,0 +1,15 @@
+uniform sampler2D jvr_Texture0;
+uniform sampler2D jvr_Texture1;
+
+varying vec2 texCoord;
+
+vec4 fastblur(float intensity) {
+    vec4 color = texture2DLod(jvr_Texture0, texCoord, intensity);
+    color.w = 1.0;
+    return color;
+}
+
+void main (void) {
+    //gl_FragColor = fastblur(5.5) + texture2D(jvr_Texture1, texCoord);
+    gl_FragColor = texture2D(jvr_Texture1, texCoord);
+}
---
+++ src/main/scala/org/whiskeysierra/powpow/quad.vs
@@ -0,0 +1,11 @@
+uniform mat4 jvr_ProjectionMatrix;
+
+attribute vec4 jvr_Vertex;
+attribute vec2 jvr_TexCoord;
+
+varying vec2 texCoord;
+
+void main(void) {
+    texCoord = jvr_TexCoord;
+    gl_Position = jvr_ProjectionMatrix * jvr_Vertex;
+}
