292c601f235c42acc7059bc291e7b47e6b85430a
Willi Schoenborn <schoenborn@cosmocode.de>
2011-07-10 20:55:59 +0200

    simplified message hub

--- src/main/scala/org/whiskeysierra/powpow/MessageHub.scala
+++ src/main/scala/org/whiskeysierra/powpow/MessageHub.scala
@@ -2,14 +2,8 @@ package org.whiskeysierra.powpow
 
 final class MessageHub(private val actors: Map[String, Actor]) extends Actor {
 
-    private def each(f: (Actor) => Unit) {
-        actors.values foreach {
-            f(_)
-        }
-    }
-
     private def broadcast(message: Any) {
-        each {a =>
+        actors.values foreach {a =>
             a.!(this, message)
         }
     }
@@ -20,13 +14,6 @@ final class MessageHub(private val actors: Map[String, Actor]) extends Actor {
                 broadcast(PoisonPill)
                 exit()
             case other => broadcast(other)
-        }
-    }
-
-    private def deliver(name: String, message: Any) {
-        actors.get(name) match {
-            case Some(actor) => actor ! message
-            case None => throw new IllegalStateException("Unknown actor name: " + name)
         }
     }
 
